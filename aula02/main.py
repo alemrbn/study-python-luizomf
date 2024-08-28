b0bec515c6856dc9095964e92a713672f58c0f93
ale <alemoura.zyc@gmail.com>
2024-08-28 00:12:16 -0300

    chore: add aula02

---
+++ aula02/main.py
@@ -0,0 +1,12 @@
+"""
+\n = quebra de linha
+sep='' = escolher separador de argumentos ( ' ' = default )
+end-'' = final do argumento ( '\r\n' = default )
+"""
+
+# \r\n = CRLF
+# \n = LF
+
+print(12, 34, 1011, sep=' . ', end=' #\n')
+print(56, 78, sep=' . ', end=' #\n')
+print(9, 10, sep=' . ', end=' #\n')
