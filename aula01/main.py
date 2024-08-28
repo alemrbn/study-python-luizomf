6839c8cfb0b9f2cdf88aa12f2c8eb10b06243432
ale <alemoura.zyc@gmail.com>
2024-08-27 23:47:55 -0300

    chore: add aula01

---
+++ aula01/main.py
@@ -0,0 +1,20 @@
+"""
+------------
+
+docstring - escrever o que eu quiser
+
+------------
+"""
+
+'''
+------------
+
+docstring - escrever o que eu quiser
+
+------------
+'''
+
+# permite escrever um comentário
+print(123) # na frente
+# abaixo
+print(456)
