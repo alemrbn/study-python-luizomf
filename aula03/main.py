61e1a99be659e4a834eefd164d03b2493f94f8b9
ale <alemoura.zyc@gmail.com>
2024-08-28 00:41:37 -0300

    chore: add aula03

---
+++ aula03/main.py
@@ -0,0 +1,22 @@
+"""
+python = linguagem de programação
+tipo de tipagem = dinâmica / forte
+str -> string -> texto
+strings são textos que estão dentro de aspas
+"""
+
+print(1234)
+
+# aspas simples
+print('Luiz Otávio')
+print(1, 'Luiz "Otávio"')
+
+# aspas duplas
+print("Luiz Otávio")
+print(2, "Luiz 'Otávio'")
+
+# escape
+print("Luiz \"Otávio\"")
+
+# r
+print(r'Luiz \'Otávio\'')
