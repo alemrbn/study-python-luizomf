fb52b72efff7159674e691ba004eb911ad5f11c6
ale <alemoura.zyc@gmail.com>
2024-08-31 01:21:22 -0300

    chore: add aula09

---
+++ aula09/main.py
@@ -0,0 +1,20 @@
+adicao = 10 + 10
+print('adição', adicao)
+
+subtracao = 10 - 5
+print('subtração', subtracao)
+
+multiplicacao = 10 * 5
+print('multiplicação', multiplicacao)
+
+divisao = 10 / 2.2 # float
+print('divisão', divisao)
+
+divisao_inteira = 10 // 2.2 # não exibe as casas decimais
+print('divisão inteira', divisao_inteira)
+
+exponenciacao = 2 ** 10
+print('exponenciação', exponenciacao)
+
+modulo = 55 % 2 # resto da divisão
+print('Módulo', modulo)
