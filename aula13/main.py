62fd5d02dc11cab6c04fedfba5a2498da126e868
ale <alemoura.zyc@gmail.com>
2024-09-04 19:32:04 -0300

    chore: add aula13

---
+++ aula13/main.py
@@ -0,0 +1,12 @@
+nome = 'Luiz Otávio'
+altura = 1.80
+peso = 95
+imc = peso / (altura * altura)
+
+linha_1 = f'{nome} tem altura de {altura:.2f}, pesa {peso} quilos e seu IMC é {imc:.2f}'
+
+print(linha_1)
+
+# Luiz Otávio tem 1.80 de altura,
+# pesa 95 quilos e seu IMC é
+# 29.320987654320987
