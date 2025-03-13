695436deda06da7a357f517f9dce420c37c78e65
ale <alemoura.zyc@gmail.com>
2025-03-12 22:42:22 -0300

    chore: add aula22

---
+++ aula22/main.py
@@ -0,0 +1,11 @@
+# operador lógico "not"
+# usado para inverter expressões
+# not True = False
+# not False = True
+
+# senha = input('senha: ')
+
+# if not senha:
+#   print('você não digitou nada')
+
+print(not 0)
