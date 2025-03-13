84dcb9ee8d3fc237a3f8d688e040e7cb1c68b8eb
ale <alemoura.zyc@gmail.com>
2025-03-12 22:25:03 -0300

    chore: add aula20

---
+++ aula20/main.py
@@ -0,0 +1,18 @@
+# operadores lógicos
+# and (e) or (ou) (not) não
+# and - todas as condições precisam ser verdadeiras
+# se qualquer valor for considerado falso, a expresão inteira será avaliada naquele valor
+# são considerados falsy: 0 0.0 '' False
+# também existe o tipo none que é usado para representar um não valor
+
+# opcoes = input('[E]ntrar | [S]air\n')
+# senha = input('Senha:\n')
+# senha_permitida = '123'
+
+# if opcoes == 'E' and senha == senha_permitida:
+#   print('você entrou!')
+# else:
+#   print('saindo..')
+
+# avaliação de curto circuito
+print(True and 0)
