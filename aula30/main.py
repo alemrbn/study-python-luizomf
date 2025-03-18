82ef9f8da42c20d9edcbf38dfcfb4d085512189e
ale <alemoura.zyc@gmail.com>
2025-03-18 17:39:40 -0300

    chore: add aula30

---
+++ aula30/main.py
@@ -0,0 +1,29 @@
+"""
+flag (bandeira) - marcar um local
+none = não valor
+is e is not = é ou não é (tipo, valor, identidade)
+id = identidade
+"""
+
+# v1 = 'a'
+# print(id(v1))
+
+condicao = True
+passou_no_if = None
+
+if condicao:
+  passou_no_if = True
+  print('Faça algo')
+else:
+  print('Não faça algo')
+
+# print(passou_no_if, passou_no_if is None)
+# print(passou_no_if, passou_no_if is not None)
+
+if passou_no_if is None:
+  print('Não passou no if')
+# else:
+  # print('Passou on if')
+
+if passou_no_if is not None:
+  print('Passou no if')
