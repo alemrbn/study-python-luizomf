4efdfcaa9186d5fdaceb2d93c55df92d73728d54
ale <alemoura.zyc@gmail.com>
2025-10-24 16:34:50 -0300

    chore: add aula53

---
+++ aula53/ex/ex.py
@@ -0,0 +1,14 @@
+def start():
+  lista = ['Maria', 'Helena', 'Luiz']
+  index = 0
+
+  # indexes = range(len(lista))
+  # for index in indexes:
+  #   print(index, lista[index])
+
+  # for index, nome in enumerate(lista):
+  #   print(index, nome)
+
+  for nome in lista:
+    print(index, nome)
+    index += 1
---
+++ aula53/main.py
@@ -0,0 +1,14 @@
+"""
+Exercício
+Exiba os índices da lista
+
+lista = ['Maria', 'Helena,' 'Luiz']
+
+0 Maria
+1 Helena
+2 Luiz
+"""
+
+from ex.ex import start
+
+start()
