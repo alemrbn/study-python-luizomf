53099e689fbfbed4042892a42501e96f260fc7ea
ale <alemoura.zyc@gmail.com>
2025-10-24 16:49:39 -0300

    chore: add aula55

---
+++ aula55/main.py
@@ -0,0 +1,10 @@
+"""
+Tipo tupla - uma lista imutável
+"""
+
+# tuple(nomes) - convertendo para tupla
+# list(nomes) - convertendo para lista(vetor)
+
+nomes = ('Maria', 'Helena', 'Luiz')
+print(type(nomes))
+
